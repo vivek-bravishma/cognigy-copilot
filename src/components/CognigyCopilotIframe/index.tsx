5b8f4005157125208cb7526b486329990c6a1dec
ViveK <vivekn@bravishma.com>
2024-02-01 16:01:26 +0530

    some changes

--- src/components/CognigyCopilotIframe/index.tsx
+++ src/components/CognigyCopilotIframe/index.tsx
@@ -20,116 +20,121 @@ const CognigyCopilotIframe = () => {
     // console.log('CognigyCopilot getInteractionData=======> ', interactionData);
 
     useEffect(() => {
-        // let interData = widgetApi?.getInteractionData();
-        console.log('CognigyCopilot getInteractionData=======> ', apiInteractionData);
-        console.log('CognigyCopilot getInteractionData type=======> ', typeof apiInteractionData);
+        if (apiInteractionData !== null || apiInteractionData !== undefined) {
+            // let interData = widgetApi?.getInteractionData();
+            console.log('CognigyCopilot getInteractionData=======> ', apiInteractionData);
+            console.log(
+                'CognigyCopilot getInteractionData type=======> ',
+                typeof apiInteractionData,
+            );
 
-        const interactionChannel = apiInteractionData?.channel;
+            const interactionChannel = apiInteractionData?.channel;
 
-        // if (interactionChannel === 'VOICE') {
-        //     let intId = apiInteractionData?.originatingAddress;
-        //     let data = JSON.stringify({
-        //         dataSource: 'mongodb-atlas',
-        //         database: 'avayaocf-qnamaker',
-        //         collection: 'cognigy-copilot',
-        //         filter: {
-        //             _id: intId,
-        //         },
-        //     });
+            // if (interactionChannel === 'VOICE') {
+            //     let intId = apiInteractionData?.originatingAddress;
+            //     let data = JSON.stringify({
+            //         dataSource: 'mongodb-atlas',
+            //         database: 'avayaocf-qnamaker',
+            //         collection: 'cognigy-copilot',
+            //         filter: {
+            //             _id: intId,
+            //         },
+            //     });
 
-        //     let config = {
-        //         method: 'post',
-        //         maxBodyLength: Infinity,
-        //         url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         data: data,
-        //     };
+            //     let config = {
+            //         method: 'post',
+            //         maxBodyLength: Infinity,
+            //         url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
+            //         headers: {
+            //             'Content-Type': 'application/json',
+            //         },
+            //         data: data,
+            //     };
 
-        //     axios
-        //         .request(config)
-        //         .then((response) => {
-        //             console.log(JSON.stringify(response.data));
-        //         })
-        //         .catch((error) => {
-        //             console.log(error);
-        //         });
-        // } else {
-        //     let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
+            //     axios
+            //         .request(config)
+            //         .then((response) => {
+            //             console.log(JSON.stringify(response.data));
+            //         })
+            //         .catch((error) => {
+            //             console.log(error);
+            //         });
+            // } else {
+            //     let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
 
-        //     console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
+            //     console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
 
-        //     if (ENGAGEMENT_PARAMETERS) {
-        //         let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
-        //         let urlToken = engObj?.urlToken;
-        //         let userId = engObj?.userId;
-        //         let sessionId = engObj?.sessionId;
-        //         let copilot = engObj?.copilot;
-        //         console.log('Engagement object==> ', engObj);
+            //     if (ENGAGEMENT_PARAMETERS) {
+            //         let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
+            //         let urlToken = engObj?.urlToken;
+            //         let userId = engObj?.userId;
+            //         let sessionId = engObj?.sessionId;
+            //         let copilot = engObj?.copilot;
+            //         console.log('Engagement object==> ', engObj);
 
-        //         console.log('Engagement engObj?.urlToken==> ', urlToken);
-        //         console.log('Engagement engObj?.userId==> ', userId);
-        //         console.log('Engagement engObj?.sessionId==> ', sessionId);
-        //         console.log('Engagement engObj?.copilot==> ', copilot);
-        //         console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
+            //         console.log('Engagement engObj?.urlToken==> ', urlToken);
+            //         console.log('Engagement engObj?.userId==> ', userId);
+            //         console.log('Engagement engObj?.sessionId==> ', sessionId);
+            //         console.log('Engagement engObj?.copilot==> ', copilot);
+            //         console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
 
-        //         setCopilotUrl(copilot);
-        //     }
-        // }
+            //         setCopilotUrl(copilot);
+            //     }
+            // }
 
-        let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
+            let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
 
-        console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
+            console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
 
-        if (ENGAGEMENT_PARAMETERS) {
-            let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
-            let urlToken = engObj?.urlToken;
-            let userId = engObj?.userId;
-            let sessionId = engObj?.sessionId;
-            let copilot = engObj?.copilot;
-            console.log('Engagement object==> ', engObj);
+            if (ENGAGEMENT_PARAMETERS) {
+                let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
+                let urlToken = engObj?.urlToken;
+                let userId = engObj?.userId;
+                let sessionId = engObj?.sessionId;
+                let copilot = engObj?.copilot;
+                console.log('Engagement object==> ', engObj);
 
-            console.log('Engagement engObj?.urlToken==> ', urlToken);
-            console.log('Engagement engObj?.userId==> ', userId);
-            console.log('Engagement engObj?.sessionId==> ', sessionId);
-            console.log('Engagement engObj?.copilot==> ', copilot);
-            console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
+                console.log('Engagement engObj?.urlToken==> ', urlToken);
+                console.log('Engagement engObj?.userId==> ', userId);
+                console.log('Engagement engObj?.sessionId==> ', sessionId);
+                console.log('Engagement engObj?.copilot==> ', copilot);
+                console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
 
-            setCopilotUrl(copilot);
-        } else {
-            let intId = apiInteractionData?.originatingAddress;
-            let data = JSON.stringify({
-                dataSource: 'mongodb-atlas',
-                database: 'avayaocf-qnamaker',
-                collection: 'cognigy-copilot',
-                filter: {
-                    _id: intId,
-                },
-            });
-
-            let config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: data,
-            };
-
-            axios
-                .request(config)
-                .then((response) => {
-                    // console.log(JSON.stringify(response.data));
-                    console.log('Copilot api response==> ', response.data);
-                    let copilotUrl = response.data.documents[0].copilot;
-                    console.log('Copilot url through api==> ', copilotUrl);
-                    setCopilotUrl(copilotUrl);
-                })
-                .catch((error) => {
-                    console.log('Error fetching copilot url from api==> ', error);
+                setCopilotUrl(copilot);
+            } else {
+                let intId = apiInteractionData?.originatingAddress;
+                let data = JSON.stringify({
+                    dataSource: 'mongodb-atlas',
+                    database: 'avayaocf-qnamaker',
+                    collection: 'cognigy-copilot',
+                    filter: {
+                        _id: intId,
+                    },
                 });
+
+                let config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: data,
+                };
+
+                axios
+                    .request(config)
+                    .then((response) => {
+                        // console.log(JSON.stringify(response.data));
+                        console.log('Copilot api response==> ', response.data);
+                        let copilotUrl = response.data.documents[0].copilot;
+                        console.log('Copilot url through api==> ', copilotUrl);
+                        setCopilotUrl(copilotUrl);
+                    })
+                    .catch((error) => {
+                        console.log('Error fetching copilot url from api==> ', error);
+                    });
+            }
         }
     }, [apiInteractionData]);
 
