76349477850a4296367fc4dfc4401d828447b62a
ViveK <vivekn@bravishma.com>
2024-08-07 18:23:53 +0530

    iframe post message to child iframe

--- src/components/CognigyCopilotIframe/index.tsx
+++ src/components/CognigyCopilotIframe/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useWidgetApi } from '../../contexts/WidgetApiContext';
 
 import axios from 'axios';
@@ -6,6 +6,7 @@ import axios from 'axios';
 const CognigyCopilotIframe = () => {
     const [copilotUrl, setCopilotUrl] = useState('');
     const { widgetApi, interactionId, apiInteractionData } = useWidgetApi();
+    const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
     console.log(
         'wipi=> ',
@@ -19,12 +20,33 @@ const CognigyCopilotIframe = () => {
     // let interData = widgetApi?.getInteractionData();
     // console.log('CognigyCopilot getInteractionData=======> ', interactionData);
 
-    function handleMessage(event) {
-        alert('Message from inner iframe: ' + event.data);
-        console.log('Message from inner iframe: ', event.data);
-    } // Add event listener for message from inner iframeswindow.addEventListener("message", handleMessage, false);
+    // function handleMessage(event) {
+    //     alert('Message from inner iframe: ' + event.data);
+    //     console.log('Message from inner iframe: ', event.data);
+    // } // Add event listener for message from inner iframeswindow.addEventListener("message", handleMessage, false);
 
-    (window as any).handleMessage = handleMessage;
+    // (window as any).handleMessage = handleMessage;
+
+    useEffect(() => {
+        const iframe = iframeRef.current;
+        if (iframe) {
+            const sendApiToIframe = () => {
+                if (iframe && iframe.contentWindow) {
+                    iframe.contentWindow.postMessage(
+                        { type: 'SET_WIDGET_API', api: widgetApi },
+                        copilotUrl,
+                    );
+                    console.log('message posted from cognigy copilot to copilot iframe ');
+                }
+            };
+
+            iframe.addEventListener('load', sendApiToIframe);
+
+            return () => {
+                iframe.removeEventListener('load', sendApiToIframe);
+            };
+        }
+    }, [copilotUrl]);
 
     useEffect(() => {
         if (apiInteractionData !== null && apiInteractionData !== undefined) {
@@ -38,60 +60,6 @@ const CognigyCopilotIframe = () => {
             const interactionChannel = apiInteractionData?.channel;
 
             console.log('is interaction channel voice?==> ', interactionChannel === 'VOICE');
-
-            // if (interactionChannel === 'VOICE') {
-            //     let intId = apiInteractionData?.originatingAddress;
-            //     let data = JSON.stringify({
-            //         dataSource: 'mongodb-atlas',
-            //         database: 'avayaocf-qnamaker',
-            //         collection: 'cognigy-copilot',
-            //         filter: {
-            //             _id: intId,
-            //         },
-            //     });
-
-            //     let config = {
-            //         method: 'post',
-            //         maxBodyLength: Infinity,
-            //         url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
-            //         headers: {
-            //             'Content-Type': 'application/json',
-            //         },
-            //         data: data,
-            //     };
-
-            //     axios
-            //         .request(config)
-            //         .then((response) => {
-            //             console.log(JSON.stringify(response.data));
-            //         })
-            //         .catch((error) => {
-            //             console.log(error);
-            //         });
-            // } else {
-            //     let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
-
-            //     console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
-
-            //     if (ENGAGEMENT_PARAMETERS) {
-            //         let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
-            //         let urlToken = engObj?.urlToken;
-            //         let userId = engObj?.userId;
-            //         let sessionId = engObj?.sessionId;
-            //         let copilot = engObj?.copilot;
-            //         console.log('Engagement object==> ', engObj);
-
-            //         console.log('Engagement engObj?.urlToken==> ', urlToken);
-            //         console.log('Engagement engObj?.userId==> ', userId);
-            //         console.log('Engagement engObj?.sessionId==> ', sessionId);
-            //         console.log('Engagement engObj?.copilot==> ', copilot);
-            //         console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
-
-            //         setCopilotUrl(copilot);
-            //     }
-            // }
-
-            ////////
 
             if (interactionChannel === 'VOICE') {
                 // let intId = apiInteractionData?.originatingAddress;
@@ -158,63 +126,6 @@ const CognigyCopilotIframe = () => {
                     setCopilotUrl(copilot);
                 }
             }
-
-            //////
-
-            // let ENGAGEMENT_PARAMETERS = apiInteractionData?.intrinsics?.ENGAGEMENT_PARAMETERS;
-
-            // console.log('ENGAGEMENT_PARAMETERS --->', ENGAGEMENT_PARAMETERS);
-
-            // if (ENGAGEMENT_PARAMETERS) {
-            //     let engObj = JSON.parse(ENGAGEMENT_PARAMETERS);
-            //     let urlToken = engObj?.urlToken;
-            //     let userId = engObj?.userId;
-            //     let sessionId = engObj?.sessionId;
-            //     let copilot = engObj?.copilot;
-            //     console.log('Engagement object==> ', engObj);
-
-            //     console.log('Engagement engObj?.urlToken==> ', urlToken);
-            //     console.log('Engagement engObj?.userId==> ', userId);
-            //     console.log('Engagement engObj?.sessionId==> ', sessionId);
-            //     console.log('Engagement engObj?.copilot==> ', copilot);
-            //     console.log('Engagement engObj?.copilot type ==> ', typeof copilot);
-
-            //     setCopilotUrl(copilot);
-            // }
-            // else {
-            //     let intId = apiInteractionData?.originatingAddress;
-            //     let data = JSON.stringify({
-            //         dataSource: 'mongodb-atlas',
-            //         database: 'avayaocf-qnamaker',
-            //         collection: 'cognigy-copilot',
-            //         filter: {
-            //             _id: intId,
-            //         },
-            //     });
-
-            //     let config = {
-            //         method: 'post',
-            //         maxBodyLength: Infinity,
-            //         url: 'https://data.mongodb-api.com/app/avayaocf-vrral/endpoint/data/v1/action/find',
-            //         headers: {
-            //             'Content-Type': 'application/json',
-            //         },
-            //         data: data,
-            //     };
-
-            //     axios
-            //         .request(config)
-            //         .then((response) => {
-            //             // console.log(JSON.stringify(response.data));
-            //             console.log('Copilot api response==> ', response.data);
-            //             let copilotUrl = response.data.documents[0].copilot;
-            //             console.log('Copilot url through api==> ', copilotUrl);
-            //             setCopilotUrl(copilotUrl);
-            //         })
-            //         .catch((error) => {
-            //             console.log('Error fetching copilot url from api==> ', error);
-            //         });
-            // }
         }
     }, [apiInteractionData]);
 
@@ -232,6 +143,7 @@ const CognigyCopilotIframe = () => {
         >
             {copilotUrl && (
                 <iframe
+                    ref={iframeRef}
                     className={'screen'}
                     title='Agent Session'
                     frameBorder={0}
